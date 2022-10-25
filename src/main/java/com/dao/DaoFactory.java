c85e77ca8c253defa6b996990b924e220a103aea
chanminmaxscaler <jcm04@naver.com>
2022-10-25 11:43:49 +0900

    [Add] DataSource 사용 [Delete] ConnectionMaker 종류 삭제

--- src/main/java/com/dao/DaoFactory.java
+++ src/main/java/com/dao/DaoFactory.java
@@ -2,23 +2,44 @@ package com.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
 
 @Configuration
 public class DaoFactory {
     @Bean
     public UserDao awsUserDao() {
-        return new UserDao(awsConnectionMaker());
+        return new UserDao(awsDataSource());
     }
     @Bean
     public UserDao LocalUserDao() {
-        return new UserDao(localConnectionMaker());
+        return new UserDao(localDataSource());
     }
+
     @Bean
-    public ConnectionMaker awsConnectionMaker() {
-        return new AwsConnectionMaker();
+    public DataSource awsDataSource() {
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
     }
+
     @Bean
-    public ConnectionMaker localConnectionMaker() {
-        return new LocalConnectionMaker();
+    public DataSource localDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("local_url");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        return dataSource;
     }
 }
--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -2,19 +2,20 @@ package com.dao;
 
 import com.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Map;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -27,7 +28,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
         ps.setString(1, id);
