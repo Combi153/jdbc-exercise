161b353aa33e58ffe02af1a3276cffc3fb7e048e
chanminmaxscaler <jcm04@naver.com>
2022-10-25 11:12:12 +0900

    [Add] 초난감 Dad and Test

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,4 +1,47 @@
 package com.dao;
 
+import com.domain.User;
+
+import java.sql.*;
+import java.util.Map;
+
 public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
+
+        PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
+
+        PreparedStatement ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+
+    }
 }
--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -1,7 +1,23 @@
 package com.dao;
 
+import com.domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
+    @Test
+    void addAndGet() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User ("1", "chanmin", "1123");
+        userDao.add(user);
+
+        User selectedUser = userDao.get(user.getId());
+        assertEquals(user.getId(), selectedUser.getId());
+        assertEquals(user.getName(), selectedUser.getName());
+        assertEquals(user.getPassword(), selectedUser.getPassword());
+    }
 }
