cce5087df8d4ec9a84db225dc40c1956b26539c6
chanminmaxscaler <jcm04@naver.com>
2022-10-25 11:31:43 +0900

    [Add] 스프링의 IoC 사용

--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -2,16 +2,25 @@ package com.dao;
 
 import com.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new DaoFactory().awsUserDao();
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         User user = new User ("1", "chanmin", "1123");
         userDao.add(user);
 
