b573cf8a01b88eef2d19ab22c101febad23ff88c
chanminmaxscaler <jcm04@naver.com>
2022-10-25 13:19:31 +0900

    [Add] Testcode 추가

--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -1,10 +1,12 @@
 package com.dao;
 
 import com.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,21 +19,62 @@ class UserDaoTest {
 
     @Autowired
     ApplicationContext context;
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+    @BeforeEach
+    public void setUp(){
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+        this.user1 = new User("1", "Han", "1234");
+        this.user2 = new User("2", "Kim", "4312");
+        this.user3 = new User("3", "Lee", "1242");
+    }
 
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
-        User user = new User ("1", "chanmin", "1123");
-
-        userDao.add(user);
+        userDao.add(user1);
         assertEquals(1, userDao.getCount());
 
-        User selectedUser = userDao.get(user.getId());
-        assertEquals(user.getId(), selectedUser.getId());
-        assertEquals(user.getName(), selectedUser.getName());
-        assertEquals(user.getPassword(), selectedUser.getPassword());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+
+        User selectedUser1 = userDao.get(user1.getId());
+        assertEquals(user1.getName(), selectedUser1.getName());
+        assertEquals(user1.getPassword(), selectedUser1.getPassword());
+
+        User selectedUser2 = userDao.get(user2.getId());
+        assertEquals(user2.getName(), selectedUser2.getName());
+        assertEquals(user2.getPassword(), selectedUser2.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void getUserFailure() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+                userDao.get(user2.getId());
+        });
     }
 }
